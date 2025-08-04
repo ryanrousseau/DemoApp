c21cfa6b747ddf5998f41b1e75fd6cd6d0b11b33
Ryan Rousseau <r@ryanrousseau.com>
2025-08-04 10:40:43 -0500

    Commit initial app version

---
+++ API/VersionDetails.cs
@@ -0,0 +1,11 @@
+namespace API;
+
+public class VersionDetails
+{
+    public string? ApplicationName { get; set; }
+    public string? DeploymentLink { get; set; }
+    public string? DeploymentTime { get; set; }
+    public string? Environment { get; set; }
+    public string? Image { get; set; }
+    public string? Release { get; set; }
+}
