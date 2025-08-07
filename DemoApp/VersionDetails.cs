ea5d4963db4ae46b4ca982990806077ff7b69e9b
Ryan Rousseau <r@ryanrousseau.com>
2025-08-07 14:30:38 -0500

    Format code

--- DemoApp/VersionDetails.cs
+++ DemoApp/VersionDetails.cs
@@ -2,6 +2,7 @@ namespace DemoApp;
 
 public class VersionDetails
 {
+    public string? ApplicationName { get;set; }
     public string? Image { get; set; }
     public string? Release { get; set; }
     public string? Version { get; set; }
